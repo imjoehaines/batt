f9eaf4f39dc58de1133190ba36740820376ff968
Joe Haines <hello@joehaines.co.uk>
2020-05-02 22:47:03 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,17 @@
+.POSIX:
+.SUFFIXES:
+
+CC ?= cc
+CFLAGS = -Wall -Wextra -pedantic-errors -Wno-padded -std=c17
+
+TARGET_EXEC ?= batt
+
+.PHONY: all
+all: build/$(TARGET_EXEC)
+
+build/$(TARGET_EXEC): build/batt.o
+	$(CC) $(LDFLAGS) $^ -o $@
+
+build/batt.o: batt.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
---
+++ batt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *handle = fopen("/sys/class/power_supply/BAT0/capacity", "r");
+
+    if (!handle) {
+        perror("fopen()");
+        return 1;
+    }
+
+    int percentage = -1;
+
+    int success = fscanf(handle, "%d", &percentage);
+
+    if (!success) {
+        perror("fscanf()");
+        return 1;
+    }
+
+    if (percentage < 0 || percentage > 100) {
+        printf("Bad percentage: '%d'\n", percentage);
+        return 1;
+    }
+
+    printf("%d\n", percentage);
+
+    return 0;
+}
